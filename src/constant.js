7d7a4174bfccfa5a638b20b88aba55ec4bae2fcb
Taekmin Kim <tantara.tm@gmail.com>
2016-10-17 00:06:40 +0900

    before facebook removal

--- src/constant.js
+++ src/constant.js
@@ -6,6 +6,7 @@ module.exports = {
     API: "http://deepcheck.herokuapp.com",
     WEB: "http://deepcheck.herokuapp.com"
     //WEB: "http://172.20.10.6:3000"
+    //WEB: "http://localhost:3000"
   },
   //SERVER: "http://deepcheck.herokuapp.com",
   KEYS: {
